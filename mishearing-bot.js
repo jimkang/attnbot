1e273c24ac0c5ff67d18d9a3fea8f40a911e26da
Jim Kang <jimkang@gmail.com>
2015-09-26 00:48:26 -0400

    Nominally working.

--- mishearing-bot.js
+++ mishearing-bot.js
@@ -2,23 +2,55 @@ var config = require('./config');
 var callNextTick = require('call-next-tick');
 var Twit = require('twit');
 var async = require('async');
+var MishearPhrase = require('mishear-phrase');
+var probable = require('probable');
 
 var dryRun = false;
 if (process.argv.length > 2) {
   dryRun = (process.argv[2].toLowerCase() == '--dry');
 }
 
-var twit = new Twit(config.twitter);
-// var wordnok = createWordnok({
-//   apiKey: config.wordnikAPIKey,
-//   logger: {
-//     log: function noOp() {}
-//   }
-// });
+var mishearPhrase = MishearPhrase({
+  shouldMishearWord: function shouldMishearWord(word, done) {
+    callNextTick(done, null, word.length < 8 || probable.roll(3) === 0);
+  },
+  pickMishearing: function pickMishearing(mishearings, done) {
+    callNextTick(done, null, probable.pickFromArray(mishearings));
+  }
+});
 
+var twit = new Twit(config.twitter);
 var stream = twit.stream('user');
 
-stream.on('tweet', function (tweet) {
-  console.log(tweet.user.screen_name);
+stream.on('tweet', reactToTweet);
+
+function reactToTweet(tweet) {
+  // console.log(tweet.user.screen_name);
   console.log(tweet.text);
-});
+  attemptToMishear(tweet.text, tweet.user.screen_name);
+}
+
+function attemptToMishear(text, username) {
+  mishearPhrase(text, packageMishearing);
+
+  function packageMishearing(error, mishearing) {
+    if (error) {
+      console.log(error);
+    }
+    else if (text === mishearing) {
+      console.log('Could not mishear', text);
+    }
+    else {
+      var response = 'OH ' + username + ': ' + mishearing;
+      if (dryRun) {
+        console.log(response);
+      }
+      else {
+        var body = {
+          status: response
+        };
+        twit.post('statuses/update', body, done);
+      }
+    }
+  }
+}
