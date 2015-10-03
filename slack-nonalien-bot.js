879ca0a09ac723ffd24a8c4ff6be418e348c72a3
Jim Kang <jimkang@gmail.com>
2015-10-02 22:51:27 -0400

    Added Slack bot.

---
+++ slack-nonalien-bot.js
@@ -0,0 +1,47 @@
+var createMicroserver = require('slack-webhook-microserver');
+var callNextTick = require('call-next-tick');
+var mishearText = require('./mishear-text');
+var decorateMishearing = require('./decorate-mishearing');
+
+var port = process.env.PORT;
+if (!port) {
+  port = 5678;
+}
+
+var lastThingSaid;
+
+function getResponse(params, done) {
+  console.log('params:', JSON.stringify(params, null, '  '));
+
+  if (params.user_name === 'slackbot' && lastThingSaid === params.text) {
+    callNextTick(done);
+  }
+  else {
+    mishearText(params.text, respondWithMishearing);
+  }
+
+  function respondWithMishearing(error, mishearing) {
+    if (error) {
+      done(error);
+    }
+    else {
+      var response = {
+        text: mishearing // decorateMishearing(mishearing)
+      };
+      var shouldDelay = (lastThingSaid !== undefined);
+      lastThingSaid = response.text;
+
+      function callDone() {
+        done(error, response);
+      }
+      setTimeout(callDone, shouldDelay ? 5000 : 0);
+    }
+  }
+}
+
+var server = createMicroserver({
+  validWebhookTokens: ['j8usmz1wCSdHJf1edOhsJp0q'],
+  getResponseObject: getResponse,
+  port: port,
+  log: console.log
+});
